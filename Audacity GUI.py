7c6ecbafa946b4f0796930baf408f27d2b19efee
Dev(ComputerAuditor) <117805200+computerauditor@users.noreply.github.com>
2023-06-13 20:57:18 +0530

    GUI Edition of Audacity
    A single GUI file using tkinter to seamlessly download YouTube video, audio and playlist 
    Requirement: pytube, tkinter
    Note: You can Simply run this file, no need for main.py or utube_playlist.py , utube_audio.py & utube_video.py

---
+++ Audacity GUI.py
@@ -0,0 +1,98 @@
+from tkinter import Tk, Label, Entry, Button, filedialog
+from pytube import YouTube, Playlist
+import datetime
+
+def download_video():
+    url = entry.get()
+    save_path = filedialog.askdirectory()
+    
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"{yt.title}_{timestamp}.mp4"
+        stream.download(output_path=save_path, filename=filename)
+        status_label.config(text=f"Video '{yt.title}' downloaded successfully.")
+    except Exception as e:
+        status_label.config(text=f"Failed to download video. Error: {str(e)}")
+
+def download_audio():
+    url = entry.get()
+    save_path = filedialog.askdirectory()
+    
+    try:
+        yt = YouTube(url)
+        try:
+            print('[+]downloading audio at *ITAG-140...*\n')
+            downloader = yt.streams.get_by_itag(140)
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"{yt.title}_{timestamp}.mp3"
+            downloader.download(output_path=save_path, filename=filename)
+        except:
+            try:
+                print('\n[+] Audio ITAG-140 not available hence trying ITAG-139... \n')
+                print('[+] Or the user pressed "CTRL+C"... \n')
+                print('[+]downloading audio at *ITAG-139*\n')
+                downloader = yt.streams.get_by_itag(139)
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                filename = f"{yt.title}_{timestamp}.mp3"
+                downloader.download(output_path=save_path, filename=filename)
+            except:
+                print('\n[+] Audio ITAG-139 not available hence trying ITAG-251... \n')
+                print('[+] Or the user pressed "CTRL+C"... \n')
+                print('[+]downloading audio at *ITAG-251*\n')
+                downloader = yt.streams.get_by_itag(251)
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                filename = f"{yt.title}_{timestamp}.mp3"
+                downloader.download(output_path=save_path, filename=filename)
+        status_label.config(text=f"Audio '{yt.title}' downloaded successfully.")
+    except Exception as e:
+        status_label.config(text=f"Failed to download audio. Error: {str(e)}")
+
+def download_playlist():
+    url = entry.get()
+    save_path = filedialog.askdirectory()
+    
+    try:
+        playlist = Playlist(url)
+        print(f'\n[+]Title => {playlist.title}\n')
+        print(f'Downloading Playlist => {playlist.title}')
+        x = 1
+        for video in playlist.videos:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"{video.title}_{timestamp}.mp4"
+            video.streams.get_highest_resolution().download(output_path=save_path, filename=filename)
+            print(f'Video No. {x} of {playlist.title} Downloaded')
+            x += 1
+        status_label.config(text=f"Playlist '{playlist.title}' downloaded successfully.")
+    except Exception as e:
+        status_label.config(text=f"Failed to download playlist. Error: {str(e)}")
+
+# Create the main window
+window = Tk()
+window.title("YouTube Downloader")
+window.configure(bg="black")
+window.geometry("400x200")  # Set the window size (width x height)
+
+# URL entry
+url_label = Label(window, text="Enter URL:", fg="white", bg="black")
+url_label.pack()
+entry = Entry(window)
+entry.pack()
+
+# Download buttons
+video_button = Button(window, text="Download Video", fg="black", bg="yellow", command=download_video)
+video_button.pack()
+
+audio_button = Button(window, text="Download Audio", fg="black", bg="yellow", command=download_audio)
+audio_button.pack()
+
+playlist_button = Button(window, text="Download Playlist", fg="black", bg="yellow", command=download_playlist)
+playlist_button.pack()
+
+# Status label
+status_label = Label(window, text="", fg="white", bg="black")
+status_label.pack()
+
+# Run the GUI
+window.mainloop()
